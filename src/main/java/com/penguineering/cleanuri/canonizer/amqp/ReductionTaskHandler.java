df2d66fda2813490b2f0ff5fb2232db0aacc6717
Stefan Haun <mail@tuxathome.de>
2022-07-25 20:19:20 +0200

    :hammer: Refactor ReductionTaskHandler for the commons library

--- src/main/java/com/penguineering/cleanuri/canonizer/amqp/ReductionTaskHandler.java
+++ src/main/java/com/penguineering/cleanuri/canonizer/amqp/ReductionTaskHandler.java
@@ -1,9 +1,9 @@
 package com.penguineering.cleanuri.canonizer.amqp;
 
 import com.penguineering.cleanuri.canonizer.processors.Canonizer;
-import com.penguineering.cleanuri.canonizer.tasks.ErrorResult;
-import com.penguineering.cleanuri.canonizer.tasks.ReductionTask;
-import com.penguineering.cleanuri.canonizer.tasks.RetrievalTask;
+import com.penguineering.cleanuri.common.amqp.ExtractionTaskEmitter;
+import com.penguineering.cleanuri.common.message.ExtractionTask;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.rabbitmq.annotation.Queue;
 import io.micronaut.rabbitmq.annotation.RabbitListener;
 import io.micronaut.rabbitmq.annotation.RabbitProperty;
@@ -16,11 +16,11 @@ import java.util.Optional;
 
 @RabbitListener
 public class ReductionTaskHandler {
-    @Inject
-    RetrievalTaskEmitter retrievalTaskEmitter;
+    @Property(name = "canonizer.retrieval-task-queue")
+    String taskQueue;
 
     @Inject
-    ErrorEmitter errorEmitter;
+    ExtractionTaskEmitter emitter;
 
     @Inject
     List<Canonizer> canonizers;
@@ -28,25 +28,22 @@ public class ReductionTaskHandler {
     @Queue("${canonizer.reduction-task-queue}")
     public void receive(@RabbitProperty("correlationId") String correlationId,
                         @RabbitProperty("replyTo") String replyTo,
-                        final ReductionTask reductionTask,
+                        final ExtractionTask task,
                         RabbitAcknowledgement acknowledgement) {
-        final RetrievalTask.Builder retrievalTaskBuilder = RetrievalTask.Builder
-                .fromReductionTask(reductionTask);
+        final ExtractionTask.Builder taskBuilder = ExtractionTask.Builder.copy(task);
 
         Optional<Canonizer> canonizer = canonizers.stream()
-                .filter(c -> c.isSuitable(reductionTask.getUri()))
+                .filter(c -> c.isSuitable(task.getRequest().getURI()))
                 .findAny();
 
         if (canonizer.isPresent()) {
-            final URI reduced = canonizer.get().canonize(reductionTask.getUri());
-            retrievalTaskBuilder.setReducedURI(reduced);
-            retrievalTaskEmitter.send(correlationId, replyTo, retrievalTaskBuilder.instance());
+            final URI canonized = canonizer.get().canonize(task.getRequest().getURI());
+            taskBuilder.setCanonizedURI(canonized);
         } else {
-            errorEmitter.send(correlationId, replyTo, new ErrorResult(
-                    404, "Could not find a matching canonizer!"
-            ));
+            taskBuilder.addError("Could not find a matching canonizer!");
         }
 
+        emitter.send(taskQueue, correlationId, replyTo, taskBuilder.instance());
         acknowledgement.ack();
     }
 }
